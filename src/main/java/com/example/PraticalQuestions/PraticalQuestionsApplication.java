fef299bbe4d435c32aa855e288db6f2ad31dbe6c
shravan <kumbhar.shravan02@gmail.com>
2026-06-08 20:09:15 +0530

    first commit

---
+++ src/main/java/com/example/PraticalQuestions/PraticalQuestionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.PraticalQuestions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraticalQuestionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraticalQuestionsApplication.class, args);
+	}
+
+}
