4c4f7750b09003239b8ed43c17804f84ba0d7fb9
shravan <kumbhar.shravan02@gmail.com>
2026-06-18 21:02:07 +0530

    priority task

---
+++ src/main/java/com/example/PraticalQuestions/D.java
@@ -0,0 +1,12 @@
+package com.example.PraticalQuestions;
+
+public class D {
+
+	public static void prog1() {
+		System.out.println("need to be hold becuase to another");
+	}
+	
+	public static void prog2() {
+		System.out.println("high proprity");
+	}
+}
